b26ec1abbba9d53b856cff661ec7db9edcfaf094
Marauder-0 <dhananjaysharma0201@outlook.com>
2025-12-02 21:12:09 +0530

    dj

---
+++ DMA/mergarr.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+    int n,m,*ptr,*ptr1;
+    printf("Enter Array One size: ");
+    scanf("%d",&n);
+    printf("\nEnter Array Two size: ");
+    scanf("%d",&m);
+    ptr = (int*)calloc(n,sizeof(int));
+    ptr1 = (int*)calloc(m,sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Memory Allocation Failure.\n");
+        exit(0);
+    }
+    if(ptr1 == NULL)
+    {
+        printf("Memory Allocation Failure.\n");
+        exit(0);
+    }
+    printf("Enter Values for Array One: ");
+    for(int i=0;i<n;++i)
+    {
+        scanf("%d",(ptr+i));
+    }
+    printf("\nEnter Values for Array Two: ");
+    for(int i=0;i<m;++i)
+    {
+        scanf("%d",(ptr1+i));
+    }
+    int new = n+m;
+    int*nptr;
+    nptr = (int*)calloc(new,sizeof(int));
+    if(nptr==NULL)
+    {
+        printf("Allocation Failure.\n");
+        exit(0);
+    }
+    for(int i=0;i<n;++i)
+    {
+        *(nptr+i) = *(ptr+i);
+    }
+    for(int i=0;i<m;++i)
+    {
+        *(nptr+n+i) = *(ptr1+i);
+    }
+    printf("\nMerged Array: ");
+    for(int i=0;i<new;++i)
+    {
+        printf("%d  ",*(nptr+i));
+    }
+    free(ptr);
+    free(ptr1);
+    free(nptr);
+    return 0;
+}
